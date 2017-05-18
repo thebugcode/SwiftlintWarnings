725d513e0ba981be45ec5ef21db8176d420d246b
Dan Ursu <dan.ursu@ing.nl>
2017-05-18 10:35:13 +0200

    Initial commit

---
+++ SwiftlintXcpretty/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SwiftlintXcpretty
+//
+//  Created by Dan Ursu on 5/18/17.
+//  Copyright © 2017 Webtrek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if (true) {
+            
+        }
+        
+        else
+        
+        {
+            // go false
+        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
